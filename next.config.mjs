81a5afeff0aed30f35c91c90c40c139eea62d65f
Amit <ambedkaramit176@gmail.com>
2024-07-09 19:01:01 +0530

    add project to repositories

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,18 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    images: {
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: 'images.unsplash.com',
+                port: '',
+            },
+        ]
+    },
+    // images: {
+    //     domains: ['images.unsplash.com', 'res.cloudinary.com']
+    // }
+
+};
 
 export default nextConfig;
